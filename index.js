680ff29f6390b96ee5a610946a6538378a0f78e9
Volodymyr <v.vovnyanka@gmail.com>
2016-07-05 08:56:53 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-for (var i = 0; i <= 100; i++) {
-    console.log(i);
+
+var n = Number(prompt('Enter number'))
+
+function fibonacci() {
+  var num;
+
+  if (n >= 2) {
+    num = fibonacci(n - 1) + fibonacci(n - 2);
+  } else {
+    num = n;
+  }
+
+  return num;
 }
+
+alert(fibonacci());
